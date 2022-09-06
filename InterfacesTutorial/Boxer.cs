410e2e5959c66affc1c20f6f7fff38097476ae73
Josh Scott <joshscott82@gmail.com>
2022-09-06 16:50:03 -0400

    Add project files.

---
+++ InterfacesTutorial/Boxer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesTutorial
+{
+    public class Boxer : ISpeakable  {
+
+    
+        public string Color { get; set; }
+        public string AnimalType { get; set; } = "Boxer";
+
+        public void Speak()
+        {
+            Console.WriteLine("Whoof");
+        }
+
+    }
+}
---
+++ InterfacesTutorial/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using InterfacesTutorial;
+
+Console.WriteLine("Interfaces");
+
+Boxer joe = new();
+joe.Color = "blue";
+Parrot poly = new();
+Frog bart = new();
+ISpeakable joe2 = new Boxer();
+
+List<ISpeakable> pets = new() { joe, poly, bart};
+
+foreach(ISpeakable pet in pets)
+{
+    pet.Scream();
+}
+
